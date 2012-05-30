e3606f563fb139f8d67ed3ff3b7abf3ccc8981e0
Alok Dhamanaskar <alokdhamanaskar@gmail.com>
2012-05-30 17:35:31 -0400

    Moving Code to Git

---
+++ src/pHomomorphism/Test.java
@@ -0,0 +1,21 @@
+
+package pHomomorphism;
+
+
+import static java.lang.System.out;
+
+/**
+ *
+ * @author Alok Dhamanaskar (alokd@uga.edu)
+ * @see LICENSE (MIT style license file). 
+ */
+public class Test {
+
+    public static void main(String[] args)
+    {
+
+    
+    }
+    
+    
+}
---
+++ src/pHomomorphism/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package has all the classes required to calculate p-Homomorphism between two classes.
+ * The implementation is based on the Algorithm in the paper
+ * "Graph homomorphism revisited for graph matching" : dl.acm.org/citation.cfm?id=1920986
+ */
+package pHomomorphism;
